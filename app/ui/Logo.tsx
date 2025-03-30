7ed404f466a13e7f8ebfd97bd24f8f071abd2f50
gwinjoe <godfreyjn873@gmail.com>
2025-03-30 11:41:41 +0100

    cleard database to check what was up

--- app/ui/Logo.tsx
+++ app/ui/Logo.tsx
@@ -2,6 +2,6 @@ import react from "react"
 
 export default function Logo() {
   return (
-    <div className="font-bold text-lg  text-color-foreground ">Prince of <span className="text-[var(--color-foreground)]">Forex</span></div>
+    <div className="font-bold text-lg  text-foreground ">Prince of <span className="text-background">Forex</span></div>
   )
 }
